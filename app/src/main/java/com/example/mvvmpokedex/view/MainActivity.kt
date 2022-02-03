2a913a6757c4a0187e26e3ea51bc1906690167fa
samurandy <andreschv@gmail.com>
2022-02-03 20:51:47 +0100

    Added available connection check

--- app/src/main/java/com/example/mvvmpokedex/view/MainActivity.kt
+++ app/src/main/java/com/example/mvvmpokedex/view/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.mvvmpokedex.view
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.NetworkInfo
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.launch
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mvvmpokedex.databinding.ActivityMainBinding
 import com.example.mvvmpokedex.model.PokemonProvider
 import com.example.mvvmpokedex.model.PokemonResult
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 
 
 class MainActivity : AppCompatActivity() {
@@ -26,8 +35,29 @@ class MainActivity : AppCompatActivity() {
 
     private fun loadPokemonInAdapter() {
         lifecycleScope.launch {
-            pokemonList = PokemonProvider().getPokemon()
-            binding.recyclerPokemon.adapter = AdapterRecycler(pokemonList)
+            if (isNetworkAvailable(applicationContext)) {
+
+                pokemonList = PokemonProvider().getPokemon()
+                binding.recyclerPokemon.adapter = AdapterRecycler(pokemonList)
+
+            } else {
+                toast("No connection available")
+                delay(2000).also { finish() }
+            }
         }
     }
+
+    private fun isNetworkAvailable(context: Context) =
+        (context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).run {
+            getNetworkCapabilities(activeNetwork)?.run {
+                hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)
+                        || hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
+                        || hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
+            } ?: false
+        }
+}
+
+//Extension function
+fun Context.toast(message: String, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, message, duration).show()
 }
